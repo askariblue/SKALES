3a81768214180a4e384e44896069f781960fc4f0
AskariBlue <Support@AskariBlue.com>
2022-07-07 13:22:48 +0100

    bug fixing CSP

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
     header("Content-Security-Policy: default-src 'self' https://*.microsoft.com");
+    header("Content-Security-Policy: style-src 'self'");
+    header("Content-Security-Policy: style-src-elem 'self'");
+    header("Content-Security-Policy: frame-src 'self' https://*.oaspapps.com");
     include("taskpane.html");
 ?>
